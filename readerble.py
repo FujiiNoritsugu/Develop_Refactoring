673e64e92956bd787bdad70acf8d2a7ca2dfe718
FujiiNoritsugu <quercus491@gmail.com>
2022-12-12 06:28:15 +0900

    パフォーマンスの演習分を追加

--- readerble.py
+++ readerble.py
@@ -1,10 +1,20 @@
 import numpy as np
 
+MIN_LEN_STR = 5
+MAX_LEN_STR = 10
+
 
 def print_string(param_str, column_count, row_count):
+    """文字列を行と列に繰り返し出力する
+
+    Args:
+        param_str (str): 表示する文字列
+        column_count (int): 列方向に文字列表示を繰り返す数
+        row_count (int): 行方向に文字列表示を繰り返す数
+    """
     len_str = len(param_str)
 
-    if not (5 <= len_str <= 10):
+    if not (MIN_LEN_STR <= len_str <= MAX_LEN_STR):
         raise ('文字列は5文字以上10文字以下にしてください')
 
     if not (param_str.isalpha() and param_str.isascii()):
